c84b33d59ca12f4075812d3fea1a54c2e4b7f4d6
masud520 <masud.abdi@student.moringaschool.com>
2023-10-18 13:07:51 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let main = document.querySelector('#main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+document.body.append(newHeader);
+
+
+newHeader.id = 'victory';
+let h1 = document.getElementById('victory');
+h1.innerHTML = 'Martin is the champion';
+
